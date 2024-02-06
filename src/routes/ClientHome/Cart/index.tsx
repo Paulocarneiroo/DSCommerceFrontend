a8f6a0e2475072e0787b972d90275c85abc28dc8
Paulo Carneiro <paulocarneirosegundo@gmail.com>
2024-02-06 14:06:50 -0300

    Cart mock Data

--- src/routes/ClientHome/Cart/index.tsx
+++ src/routes/ClientHome/Cart/index.tsx
@@ -1,5 +1,23 @@
 import "./styles.css";
-import computerImg from "../../../assets/computer.png";
+
+const cart = {
+    items: [
+        {
+            productId: 4,
+            quantity: 1,
+            name: "PC Gamer",
+            price: 1200,
+            imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jpg"
+        },
+        {
+            productId: 5,
+            quantity: 2,
+            name: "Rails for Dummies",
+            price: 100.99,
+            imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/5-big.jpg"
+        }
+    ]
+}
 
 
 const Cart = () => {
@@ -7,38 +25,26 @@ const Cart = () => {
         <main>
             <section id="cart-container-section" className="dsc-container">
                 <div className="dsc-card dsc-mb20">
-                    <div className="dsc-cart-item-container dsc-line-bottom">
-                        <div className="dsc-cart-item-left">
-                            <img src={computerImg} alt="Computador" />
-                            <div className="dsc-cart-item-description">
-                                <h3>Computador Gamer XT</h3>
-                                <div className="dsc-cart-item-quantity-container">
-                                    <div className="dsc-cart-item-quantity-btn">-</div>
-                                    <p>1</p>
-                                    <div className="dsc-cart-item-quantity-btn">+</div>
+                    {
+                        cart.items.map(item => (
+                            <div key={item.productId} className="dsc-cart-item-container dsc-line-bottom">
+                                <div className="dsc-cart-item-left">
+                                    <img src={item.imgUrl} alt={item.name} />
+                                    <div className="dsc-cart-item-description">
+                                        <h3>{item.name}</h3>
+                                        <div className="dsc-cart-item-quantity-container">
+                                            <div className="dsc-cart-item-quantity-btn">-</div>
+                                            <p>{item.quantity}</p>
+                                            <div className="dsc-cart-item-quantity-btn">+</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="dsc-cart-item-right">
+                                    R$ {(item.price * item.quantity).toFixed(2)}
                                 </div>
                             </div>
-                        </div>
-                        <div className="dsc-cart-item-right">
-                            R$ 5000,00
-                        </div>
-                    </div>
-                    <div className="dsc-cart-item-container dsc-line-bottom">
-                        <div className="dsc-cart-item-left">
-                            <img src={computerImg} alt="Computador" />
-                            <div className="dsc-cart-item-description">
-                                <h3>Computador Gamer XT</h3>
-                                <div className="dsc-cart-item-quantity-container">
-                                    <div className="dsc-cart-item-quantity-btn">-</div>
-                                    <p>1</p>
-                                    <div className="dsc-cart-item-quantity-btn">+</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="dsc-cart-item-right">
-                            R$ 5000,00
-                        </div>
-                    </div>
+                        ))
+                    }
                     <div className="dsc-cart-total-container">
                         <h3>R$ 15000,00</h3>
                     </div>
