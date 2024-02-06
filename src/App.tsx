e2dd5d98176f6b467eee4f74297f55a5e35c3645
Paulo Carneiro <paulocarneirosegundo@gmail.com>
2024-02-06 13:52:09 -0300

    Cart route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import ClientHome from './routes/ClientHome';
 import Catalog from './routes/ClientHome/Catalog';
 import ProductDetails from './routes/ClientHome/ProductDetails';
+import Cart from './routes/ClientHome/Cart';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
             <Route index element={<Catalog />} />
             <Route path="catalog" element={<Catalog />} />
             <Route path="product-details/:productId" element={<ProductDetails />} />
+            <Route path="cart" element={<Cart />} />
           </Route>
           <Route path="*" element={<Navigate to="/"/>}/>
         </Routes>
