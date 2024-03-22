8c2e8681e7520a87eb398eaba9220b457f2cd7e6
Paulo <paulocarneirosegundo@gmail.com>
2024-03-21 23:31:50 -0300

    Fixing some problems

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -1,88 +1,89 @@
+
 export function update(inputs: any, name: string, newValue: any) {
-    return { ...inputs, [name]: { ...inputs[name], value: newValue } };
+  return { ...inputs, [name]: { ...inputs[name], value: newValue } };
+}
+
+export function toValues(inputs: any) {
+  const data: any = {};
+  for (var name in inputs) {
+    data[name] = inputs[name].value;
   }
-  
-  export function toValues(inputs: any) {
-    const data: any = {};
-    for (var name in inputs) {
-      data[name] = inputs[name].value;
+  return data;
+}
+
+export function updateAll(inputs: any, newValues: any) {
+  const newInputs: any = {};
+  for (var name in inputs) {
+    newInputs[name] = { ...inputs[name], value: newValues[name] };
+  }
+  return newInputs;
+}
+
+export function validate(inputs: any, name: string) {
+  if (!inputs[name].validation) {
+    return inputs;
+  }
+  const isInvalid = !inputs[name].validation(inputs[name].value);
+  return {
+    ...inputs,
+    [name]: { ...inputs[name], invalid: isInvalid.toString() },
+  };
+}
+
+export function toDirty(inputs: any, name: string) {
+  return { ...inputs, [name]: { ...inputs[name], dirty: 'true' } };
+}
+
+export function updateAndValidate(inputs: any, name: string, newValue: any) {
+  const dataUpdated = update(inputs, name, newValue);
+  return validate(dataUpdated, name);
+}
+
+export function dirtyAndValidate(inputs: any, name: string) {
+  const dataDirty = toDirty(inputs, name);
+  return validate(dataDirty, name);
+}
+
+export function toDirtyAll(inputs: any) {
+  const newInputs: any = {};
+  for (var name in inputs) {
+    newInputs[name] = { ...inputs[name], dirty: 'true' };
+  }
+  return newInputs;
+}
+
+export function validateAll(inputs: any) {
+  const newInputs: any = {};
+  for (var name in inputs) {
+    if (inputs[name].validation) {
+      const isInvalid = !inputs[name].validation(inputs[name].value);
+      newInputs[name] = { ...inputs[name], invalid: isInvalid.toString() };
+    } else {
+      newInputs[name] = { ...inputs[name] };
     }
-    return data;
   }
-  
-  export function updateAll(inputs: any, newValues: any) {
-    const newInputs: any = {};
-    for (var name in inputs) {
-      newInputs[name] = { ...inputs[name], value: newValues[name] };
+  return newInputs;
+}
+
+export function dirtyAndValidateAll(inputs: any) {
+  return validateAll(toDirtyAll(inputs));
+}
+
+export function hasAnyInvalid(inputs: any) {
+  for (var name in inputs) {
+    if (inputs[name].dirty === 'true' && inputs[name].invalid === 'true') {
+      return true;
     }
-    return newInputs;
   }
-  
-  export function validate(inputs: any, name: string) {
-    if (!inputs[name].validation) {
-      return inputs;
-    }
-    const isInvalid = !inputs[name].validation(inputs[name].value);
-    return {
-      ...inputs,
-      [name]: { ...inputs[name], invalid: isInvalid.toString() },
-    };
-  }
-  
-  export function toDirty(inputs: any, name: string) {
-    return { ...inputs, [name]: { ...inputs[name], dirty: 'true' } };
-  }
-  
-  export function updateAndValidate(inputs: any, name: string, newValue: any) {
-    const dataUpdated = update(inputs, name, newValue);
-    return validate(dataUpdated, name);
-  }
-  
-  export function dirtyAndValidate(inputs: any, name: string) {
-    const dataDirty = toDirty(inputs, name);
-    return validate(dataDirty, name);
-  }
-  
-  export function toDirtyAll(inputs: any) {
-    const newInputs: any = {};
-    for (var name in inputs) {
-      newInputs[name] = { ...inputs[name], dirty: 'true' };
-    }
-    return newInputs;
-  }
-  
-  export function validateAll(inputs: any) {
-    const newInputs: any = {};
-    for (var name in inputs) {
-      if (inputs[name].validation) {
-        const isInvalid = !inputs[name].validation(inputs[name].value);
-        newInputs[name] = { ...inputs[name], invalid: isInvalid.toString() };
-      } else {
-        newInputs[name] = { ...inputs[name] };
-      }
-    }
-    return newInputs;
-  }
-  
-  export function dirtyAndValidateAll(inputs: any) {
-    return validateAll(toDirtyAll(inputs));
-  }
-  
-  export function hasAnyInvalid(inputs: any) {
-    for (var name in inputs) {
-      if (inputs[name].dirty === 'true' && inputs[name].invalid === 'true') {
-        return true;
-      }
-    }
-    return false;
-  }
-  
-  export function setBackendErrors(inputs: any, errors: any[]) {
-    const newInputs = { ...inputs };
-    errors.forEach((item) => {
-      newInputs[item.fieldName].message = item.message;
-      newInputs[item.fieldName].dirty = 'true';
-      newInputs[item.fieldName].invalid = 'true';
-    });
-    return newInputs;
-  }
+  return false;
+}
+
+export function setBackendErrors(inputs: any, errors: any[]) {
+  const newInputs = { ...inputs };
+  errors.forEach((item) => {
+    newInputs[item.fieldName].message = item.message;
+    newInputs[item.fieldName].dirty = 'true';
+    newInputs[item.fieldName].invalid = 'true';
+  });
+  return newInputs;
+}
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,41 @@
-import axios, { AxiosRequestConfig } from "axios";
-import { BASE_URL } from "./system";
+import axios, { AxiosRequestConfig } from 'axios';
+import { BASE_URL } from './system';
+import * as authService from '../services/auth-service';
+import { history } from './history';
 
-export function requestBackend(config: AxiosRequestConfig){
-    return axios({...config, baseURL: BASE_URL});
+export function requestBackend(config: AxiosRequestConfig) {
+  const headers = config.withCredentials
+    ? {
+        ...config.headers,
+        Authorization: 'Bearer ' + authService.getAccessToken(),
+      }
+    : config.headers;
+
+  return axios({ ...config, baseURL: BASE_URL, headers });
 }
+
+
+axios.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  },
+);
+
+
+axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (error.response.status === 401) {
+      history.push('/login');
+    }
+    if (error.response.status === 403) {
+      history.push('/catalog');
+    }
+    return Promise.reject(error);
+  },
+);
